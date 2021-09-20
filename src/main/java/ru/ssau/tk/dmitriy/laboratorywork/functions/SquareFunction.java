f8340ed48004acdca583071ccef68cc008cdcf09
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-09-20 21:13:15 +0400

    Создан еще один класс математических функций SquareFunction, единственныйм метод которого возвращает квадрат, переданного значения, реализует интерфейс MathFunction

---
+++ src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/SquareFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.dmitriy.laboratorywork.functions;
+
+public class SquareFunction implements MathFunction {
+    @Override
+    public double apply(double x) {
+        return x * x;
+    }
+}
