d160fee03772a8c44ee8a77778af6f902032d079
altyshkin <29455komandor@mail.ru>
2021-09-21 21:01:49 +0400

    Создан класс UnitFunction, реализующий интерфейс MathFunction и переопределяющий функцию apply согласно заданию

---
+++ src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/UnitFunction.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.dmitriy.laboratorywork.functions;
+
+public class UnitFunction extends ConstantFunction implements MathFunction {
+
+    public UnitFunction(double x){
+        super(1);
+    }
+}
+
