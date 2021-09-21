05233dfdddee0e12ea2c81ee77511912cd1b8d8a
altyshkin <29455komandor@mail.ru>
2021-09-21 20:28:06 +0400

    Создан класс CubeFunction, реализующий интерфейс MathFunction и переопределяющий функцию apply согласно заданию

---
+++ src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/CubeFunction.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.dmitriy.laboratorywork.functions;
+
+public class CubeFunction implements MathFunction {
+
+    @Override
+        public double apply(double x){
+            return x*x*x;
+        }
+
+}
