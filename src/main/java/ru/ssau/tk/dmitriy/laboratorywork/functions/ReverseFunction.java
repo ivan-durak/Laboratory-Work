0bb3451e9fdbc169003c839c7501ab9e03af2789
altyshkin <29455komandor@mail.ru>
2021-09-21 20:42:21 +0400

    Создан класс ReverseFunction, реализующий интерфейс MathFunction и переопределяющий функцию apply согласно заданию

---
+++ src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/ReverseFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.dmitriy.laboratorywork.functions;
+
+public class ReverseFunction implements MathFunction{
+    @Override
+    public double apply(double x){
+        return 1/x;
+    }
+}
