af98d899b085cd8fe65d79062c9f96687cd39ece
altyshkin <29455komandor@mail.ru>
2021-09-22 12:17:55 +0400

    Создан класс-тест UnitFunctionTest, тестирующий метод ,когорый всегда возвращает единицу не зависимо от входящего аргумента.

---
+++ src/test/java/ru/ssau/tk/dmitriy/laboratorywork/functions/UnitFunctionTest.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk.dmitriy.laboratorywork.functions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class UnitFunctionTest {
+    @Test
+    public void testOfApplyCubeFunction() {
+        double value = 5;
+        UnitFunction object = new UnitFunction(1);
+        Assert.assertEquals(object.apply(value), 1,0.00001);
+    }
+}
