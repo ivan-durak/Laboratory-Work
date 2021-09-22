2267abbfbc12ae8aa897473494e590cd5d42c608
altyshkin <29455komandor@mail.ru>
2021-09-22 12:23:16 +0400

    Создан метод CompositeFunction andThen(MathFunction afterFunction) внутри интерфейса MathFunction, который возвращает сложную функцию

--- src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/MathFunction.java
@@ -1,5 +1,11 @@
 package ru.ssau.tk.dmitriy.laboratorywork.functions;
 
 public interface MathFunction {
+
     double apply(double x);
+    default CompositeFunction andThen(MathFunction afterFunction){
+
+        return new CompositeFunction(this,afterFunction );
+
+    }
 }
