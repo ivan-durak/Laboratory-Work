c30289d29eaced9137291c215d1ae23cfef405dc
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-10-06 08:03:31 +0400

    Нет, особых изменений: отступы в AbstractTabulatedFunction и регламентирование наследования в LinkedListTabulatedFunction

--- src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/AbstractTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/AbstractTabulatedFunction.java
@@ -8,19 +8,21 @@ public abstract class AbstractTabulatedFunction implements TabulatedFunction {
     protected abstract double extrapolateRight(double x);
 
     protected abstract double interpolate(double x, int floorIndex);
-    public abstract int  getCount ();
-    public abstract double  getX ( int  index );
-    public abstract  double  getY ( int  index );
-    public abstract  void  setY ( int  index , double  value );
+
+    public abstract int getCount();
+
+    public abstract double getX(int index);
+
+    public abstract double getY(int index);
+
+    public abstract void setY(int index, double value);
 
     protected double interpolate(double x, double leftx, double rightx, double y, double lefty, double righty) {
         return (lefty + (x - leftx) * (righty - lefty) / (rightx - leftx));
 
     }
 
-
     @Override
-
     public double apply(double x) {
         if (x < leftBound()) {
             return extrapolateLeft(x);
