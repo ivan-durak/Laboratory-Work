e6b6efdef33bd7a192d3ef4f2013581fc5cfe4a1
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-10-06 11:59:36 +0400

    Реализован метод insert() интерфейса Insertable и remove() интерфейса Removable, в дальнейшем будет добавлен тесты к ним

--- src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/ArrayTabulateFunction.java
+++ src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/ArrayTabulateFunction.java
@@ -1,12 +1,13 @@
 package ru.ssau.tk.dmitriy.laboratorywork.functions;
-import  java.util.Arrays ;
 
-public  class  ArrayTabulateFunction  extends  AbstractTabulatedFunction {
+import java.util.Arrays;
+
+public class ArrayTabulateFunction extends AbstractTabulatedFunction implements Insertable, Removable {
 
     private double[] xValues;
     private double[] yValues;
     private int count = 0;
-
+    
     public ArrayTabulateFunction(double[] xValues, double[] yValues) {
 
         count = xValues.length;
@@ -33,77 +34,123 @@ public  class  ArrayTabulateFunction  extends  AbstractTabulatedFunction {
         this.yValues = Arrays.copyOf(yValues, count);
 
     }
+
     @Override
-    public  int  getCount () {
+    public int getCount() {
         return this.count;
     }
 
     @Override
-    public  double  getX ( int  index ) {
-        return xValues [index];
+    public double getX(int index) {
+        return xValues[index];
     }
 
     @Override
-    public  double  getY ( int  index ) {
-        return yValues [index];
+    public double getY(int index) {
+        return yValues[index];
     }
 
     @Override
-    public  void  setY ( int  index , double  value ) {
-        yValues [index] = value;
+    public void setY(int index, double value) {
+        yValues[index] = value;
     }
 
     @Override
-    public  double  leftBound () {
-        return xValues[ 0 ];
+    public double leftBound() {
+        return xValues[0];
     }
 
     @Override
-    public  double  rightBound () {
-        return xValues [count -  1 ];
+    public double rightBound() {
+        return xValues[count - 1];
     }
 
     @Override
-    public  int  indexOfX ( double  x ) {
-        for ( int element =  0 ; element <  this . count; element++ ) {
-            if ( Math . abs (xValues [element] - x) <  0.0001) return element;
+    public int indexOfX(double x) {
+        for (int element = 0; element < this.count; element++) {
+            if (Math.abs(xValues[element] - x) < 0.0001) return element;
         }
-        return   - 1 ;
+        return -1;
     }
 
     @Override
-    public  int  indexOfY ( double  y ) {
-        for ( int element =  0 ; element <  this . count; element ++ ) {
-            if ( Math . abs (yValues [element] - y) <  0.0001 ) return element;
+    public int indexOfY(double y) {
+        for (int element = 0; element < this.count; element++) {
+            if (Math.abs(yValues[element] - y) < 0.0001) return element;
         }
-        return   - 1 ;
+        return -1;
     }
 
     @Override
-    public  int  floorIndexOfX ( double  x ) {
-        for ( int element =  0 ; element <  this . count; element ++ ) {
-            if ( Math.abs (xValues [element] - x) <0.0001) return element ;
+    public int floorIndexOfX(double x) {
+        for (int element = 0; element < this.count; element++) {
+            if (Math.abs(xValues[element] - x) < 0.0001) return element;
         }
-        for ( int element =  0 ; element <  this.count; element ++ ) {
-            if (x < xValues [element] && element !=  0 ) return element -  1 ;
-            if  (x < xValues [element]) return  0 ;
+        for (int element = 0; element < this.count; element++) {
+            if (x < xValues[element] && element != 0) return element - 1;
+            if (x < xValues[element]) return 0;
         }
-        return   this.count;
+        return this.count;
     }
 
     @Override
-    public  double  extrapolateLeft ( double  x ) {
-        return yValues [ 0 ] + (yValues [ 1 ] - yValues [ 0 ]) * (x - xValues [ 0 ]) / (xValues [ 1 ] - xValues [ 0 ]);
+    public double extrapolateLeft(double x) {
+        return yValues[0] + (yValues[1] - yValues[0]) * (x - xValues[0]) / (xValues[1] - xValues[0]);
     }
 
     @Override
-    public  double  extrapolateRight ( double  x ) {
-        return yValues [count -  2 ] + (yValues [count -  1 ] - yValues [count -  2 ]) * (x - xValues [count -  2 ]) / (xValues[count -  1 ] - xValues [count -  2 ]);
+    public double extrapolateRight(double x) {
+        return yValues[count - 2] + ((yValues[count - 1] - yValues[count - 2]) * (x - xValues[count - 2])) / (xValues[count - 1] - xValues[count - 2]);
     }
 
     @Override
-    public   double  interpolate ( double  x , int  floorIndex ) {
-        return yValues [floorIndex] + (yValues[floorIndex +  1 ] - yValues [floorIndex]) * (x - xValues [floorIndex]) / (xValues [floorIndex +  1 ] - xValues [ 0 ]);
+    public double interpolate(double x, int floorIndex) {
+        return yValues[floorIndex] + (yValues[floorIndex + 1] - yValues[floorIndex]) * (x - xValues[floorIndex]) / (xValues[floorIndex + 1] - xValues[floorIndex]);
+    }
+
+    @Override
+    public void insert(double x, double y) {
+        if (indexOfX(x) != -1) { //если х найден в таблице
+            setY(indexOfX(x), y);//устанавливаем его значение
+        } else {                 //если не найден
+            if (xValues.length - count == 0) {
+                double[] newXValues = new double[count + 5];
+                double[] newYValues = new double[count + 5];
+                System.arraycopy(xValues, 0, newXValues, 0, count);
+                System.arraycopy(yValues, 0, newYValues, 0, count);
+                xValues = newXValues;
+                yValues = newYValues;
+            }
+            if (x < leftBound()) { //х меньше левой границы
+                double help = extrapolateLeft(x);//если мы сначала скопируем, то там будет 0 и получится неправильное значение
+                System.arraycopy(xValues, 0, xValues, 1, count);
+                System.arraycopy(yValues, 0, yValues, 1, count);
+                xValues[0] = x;
+                yValues[0] = help;
+                count++;
+                return;
+            }
+            if (x > rightBound()) {//х больше правой границы
+                xValues[count] = x;
+                yValues[count] = extrapolateRight(x);
+                count++;
+                return;
+            }  //дальше код, который выполнится при условии, что есть еще не занятые ячейки в массиве и х находится
+            int nearestIndex = floorIndexOfX(x);  //между табличными значениями х
+            double help = interpolate(x, nearestIndex);
+            System.arraycopy(xValues, nearestIndex + 1, xValues, nearestIndex + 2, (count - 1) - nearestIndex);
+            System.arraycopy(yValues, nearestIndex + 1, yValues, nearestIndex + 2, (count - 1) - nearestIndex);
+            xValues[nearestIndex + 1] = x;
+            yValues[nearestIndex + 1] = help;
+            count++;
+        }
+    }
+
+    @Override
+    public void remove(int index) {
+        System.arraycopy(xValues, index + 1, xValues, index, (count - 1) - index);
+        System.arraycopy(yValues, index + 1, yValues, index, (count - 1) - index);
+        count--;
     }
 }
 
