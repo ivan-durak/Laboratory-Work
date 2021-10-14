f357d8a170f8ca7ec3140f96dad0516be562ed1f
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-10-14 22:58:17 +0400

    Добавлен тест-метод testSetY() в класс-тест для Array, также в соответствии с замечаниями дополнены тесты для insert() и remove() в этом же тест-классе

--- src/test/java/ru/ssau/tk/dmitriy/laboratorywork/functions/ArrayTabulateFunctionTest.java
+++ src/test/java/ru/ssau/tk/dmitriy/laboratorywork/functions/ArrayTabulateFunctionTest.java
@@ -3,6 +3,8 @@ package ru.ssau.tk.dmitriy.laboratorywork.functions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 
 public class ArrayTabulateFunctionTest {
     private static final double DELTA = 0.0001;
@@ -34,6 +36,18 @@ public class ArrayTabulateFunctionTest {
         for (int element = 0; element < 99; element++) {
             Assert.assertEquals(arrayTabulateObjectTwo.getY(element), sqObject.apply(arrayTabulateObjectTwo.getX(element)), DELTA);
         }
+    }
+
+    @Test
+    public void testSetY() {
+        ArrayTabulateFunction array = new ArrayTabulateFunction(xValues, yValues);
+        for (int i = 0; i < yValues.length; i++) {
+            Assert.assertEquals(array.getY(i), yValues[i]);
+        }
+        array.setY(1, 3.9);
+        array.setY(3, 11);
+        Assert.assertEquals(array.getY(1), 3.9);
+        Assert.assertEquals(array.getY(3), 11.0);
     }
 
     @Test
@@ -88,32 +102,80 @@ public class ArrayTabulateFunctionTest {
 
     @Test
     public void testInsert() {
-        arrayTabulateObject.insert(3.4, 5); //если х найден в массиве
-        Assert.assertEquals(arrayTabulateObject.getY(1), 5.0);
-        Assert.assertEquals(arrayTabulateObject.getCount(), 4);
-        arrayTabulateObject.insert(2, 2.5);//если х меньше левой границы
-        Assert.assertEquals(arrayTabulateObject.getCount(), 5);
-        Assert.assertEquals(arrayTabulateObject.getX(0), 2.0);
-        Assert.assertEquals(arrayTabulateObject.getY(0), -2.9692307, DELTA);
-        arrayTabulateObject.insert(7, 4);  //если х больше правой границы
-        Assert.assertEquals(arrayTabulateObject.getX(5), 7.0, DELTA);
-        Assert.assertEquals(arrayTabulateObject.getY(5), 7.725, DELTA);
-        Assert.assertEquals(arrayTabulateObject.getCount(), 6);
-        arrayTabulateObject.insert(4, 2.9); //если х внутри интервала значений
-        Assert.assertEquals(arrayTabulateObject.getCount(), 7);
-        Assert.assertEquals(arrayTabulateObject.getX(3), 4.0);
-        Assert.assertEquals(arrayTabulateObject.getY(3), 4.33333, DELTA);
+        ArrayTabulateFunction array = new ArrayTabulateFunction(xValues, yValues);
+        array.insert(3.4, 5); //если х найден в массиве
+        Assert.assertEquals(array.getY(1), 5.0);
+        double[] newYValues = new double[]{-2.4, 5.0, 3, 5.1};
+        for (int i = 0; i < xValues.length; i++) {
+            Assert.assertEquals(array.getX(i), xValues[i], DELTA);
+        }
+        for (int i = 0; i < newYValues.length; i++) {
+            Assert.assertEquals(array.getY(i), newYValues[i], DELTA);
+        }
+        Assert.assertEquals(array.getCount(), 4);
+        array.insert(2, 2.5);//если х меньше левой границы
+        Assert.assertEquals(array.getCount(), 5);
+        Assert.assertEquals(array.getX(0), 2.0);
+        Assert.assertEquals(array.getY(0), -2.9692307, DELTA);
+        double[] newXValues = new double[]{2.0, 2.1, 3.4, 5.2, 6};
+        newYValues = new double[]{-2.9692, -2.4, 5.0, 3, 5.1};
+        for (int i = 0; i < newXValues.length; i++) {
+            Assert.assertEquals(array.getX(i), newXValues[i], DELTA);
+        }
+        for (int i = 0; i < newYValues.length; i++) {
+            Assert.assertEquals(array.getY(i), newYValues[i], DELTA);
+        }
+        Assert.assertEquals(array.getCount(), 5);
+        array.insert(7, 4);  //если х больше правой границы
+        Assert.assertEquals(array.getCount(), 6);
+        Assert.assertEquals(array.getX(5), 7.0, DELTA);
+        Assert.assertEquals(array.getY(5), 7.725, DELTA);
+        newXValues = new double[]{2.0, 2.1, 3.4, 5.2, 6, 7};
+        newYValues = new double[]{-2.9692, -2.4, 5.0, 3, 5.1, 7.725};
+        for (int i = 0; i < newXValues.length; i++) {
+            Assert.assertEquals(array.getX(i), newXValues[i], DELTA);
+        }
+        for (int i = 0; i < newYValues.length; i++) {
+            Assert.assertEquals(array.getY(i), newYValues[i], DELTA);
+        }
+        Assert.assertEquals(array.getCount(), 6);
+        array.insert(4, 2.9); //если х внутри интервала значений
+        Assert.assertEquals(array.getCount(), 7);
+        Assert.assertEquals(array.getX(3), 4.0);
+        Assert.assertEquals(array.getY(3), 4.33333, DELTA);
+        newXValues = new double[]{2.0, 2.1, 3.4, 4, 5.2, 6, 7};
+        newYValues = new double[]{-2.9692, -2.4, 5.0, 4.3333, 3, 5.1, 7.725};
+        for (int i = 0; i < newXValues.length; i++) {
+            Assert.assertEquals(array.getX(i), newXValues[i], DELTA);
+        }
+        for (int i = 0; i < newYValues.length; i++) {
+            Assert.assertEquals(array.getY(i), newYValues[i], DELTA);
+        }
     }
 
     @Test
     public void testRemove() {
-        double[] xValues = new double[]{2.1, 3.4, 5.2, 6};
-        double[] yValues = new double[]{-2.4, 1.2, 3, 5.1};
-        ArrayTabulateFunction arrayTabulateObjectThree = new ArrayTabulateFunction(xValues, yValues);
-        Assert.assertEquals(arrayTabulateObjectThree.getCount(), 4);
-        arrayTabulateObjectThree.remove(2);
-        Assert.assertEquals(arrayTabulateObjectThree.getCount(), 3);
-        Assert.assertEquals(arrayTabulateObjectThree.getX(2), 6.0);
-        Assert.assertEquals(arrayTabulateObjectThree.getY(2), 5.1);
+        double[] newXValues = new double[]{2.1, 3.4, 5.2, 6};
+        double[] newYValues = new double[]{-2.4, 1.2, 3, 5.1};
+        ArrayTabulateFunction array = new ArrayTabulateFunction(newXValues, newYValues);
+        Assert.assertEquals(array.getCount(), 4);
+        for (int i = 0; i < newXValues.length; i++) {
+            Assert.assertEquals(array.getX(i), newXValues[i]);
+        }
+        for (int i = 0; i < newYValues.length; i++) {
+            Assert.assertEquals(array.getY(i), newYValues[i]);
+        }
+        array.remove(2);
+        Assert.assertEquals(array.getCount(), 3);
+        Assert.assertEquals(array.getY(2), 5.1);
+        Assert.assertEquals(array.getX(2), 6.0);
+        newXValues = new double[]{2.1, 3.4, 6};
+        newYValues = new double[]{-2.4, 1.2, 5.1};
+        for (int i = 0; i < newXValues.length; i++) {
+            Assert.assertEquals(array.getX(i), newXValues[i]);
+        }
+        for (int i = 0; i < newYValues.length; i++) {
+            Assert.assertEquals(array.getY(i), newYValues[i]);
+        }
     }
 }
