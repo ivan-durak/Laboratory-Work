a342edddd9ee863047bc9adc6049ac66c877cce8
altyshkin <29455komandor@mail.ru>
2021-11-01 22:23:12 +0400

    Добавлен класс Point , так же создан новый пакет excetions и в нем созданы 3 класса исключений ArrayIsNotSortedException,DifrentLeghtOfArraysException,

---
+++ src/main/java/Exceptions/DifferentLengthOfArraysException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+public class DifferentLengthOfArraysException extends RuntimeException {
+
+    public DifferentLengthOfArraysException() {
+    }
+
+    public DifferentLengthOfArraysException(String string) {
+        super(string);
+    }
+}
