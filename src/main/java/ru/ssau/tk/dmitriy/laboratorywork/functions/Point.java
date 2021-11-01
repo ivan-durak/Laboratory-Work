0bdc1187c414bc93e1e2eddb5f6da85f1415c109
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-11-02 01:43:10 +0400

    Добавлен класс Point

---
+++ src/main/java/ru/ssau/tk/dmitriy/laboratorywork/functions/Point.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.dmitriy.laboratorywork.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
