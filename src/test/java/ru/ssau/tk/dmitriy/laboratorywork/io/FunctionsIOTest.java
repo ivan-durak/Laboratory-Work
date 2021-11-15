677011eab72b8553a56ce3106dfc49c2e0b1a796
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-11-15 11:07:03 +0400

    немного изменен тест в FunctionsIOTest, добавлена директория temp

--- src/test/java/ru/ssau/tk/dmitriy/laboratorywork/io/FunctionsIOTest.java
+++ src/test/java/ru/ssau/tk/dmitriy/laboratorywork/io/FunctionsIOTest.java
@@ -27,14 +27,15 @@ public class FunctionsIOTest {
 
     @Test
     public void testByteStreams() throws IOException {
+        TabulatedFunction tabulatedFunction = new ArrayTabulateFunction(new double[]{2, 3, 5, 7, 9}, new double[]{5, 8, 17, 21, 15, 34});
         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("temp/test byte streams.bin"));
-        FunctionsIO.writeTabulatedFunction(bufferedOutputStream, arrayTabulated);
+        FunctionsIO.writeTabulatedFunction(bufferedOutputStream, tabulatedFunction);
         BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("temp/test byte streams.bin"));
         TabulatedFunction listTabulated = FunctionsIO.readTabulatedFunction(bufferedInputStream, new LinkedListTabulatedFunctionFactory());
-        Assert.assertEquals(listTabulated.getCount(), arrayTabulated.getCount());
+        Assert.assertEquals(listTabulated.getCount(), tabulatedFunction.getCount());
         for (int index = 0; index < listTabulated.getCount(); index++) {
-            Assert.assertEquals(listTabulated.getX(index), arrayTabulated.getX(index));
-            Assert.assertEquals(listTabulated.getY(index), arrayTabulated.getY(index));
+            Assert.assertEquals(listTabulated.getX(index), tabulatedFunction.getX(index));
+            Assert.assertEquals(listTabulated.getY(index), tabulatedFunction.getY(index));
         }
     }
 
